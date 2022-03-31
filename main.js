24d9b0caf463df5cf6ba9eb1d6238cc279a88f42
DellaSantaF <71725757+DellaSantaF@users.noreply.github.com>
2022-03-31 17:14:36 +0200

    require.js + main.js stuff

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+//Main execution file
+let files = require('fs').readdirSync('/sounds');
+let audios = []
 
+for(file of files) {
+    let a = new Audio(file);
+    audios.append(a);
+}
 
 new AudioPlayer(audios);
