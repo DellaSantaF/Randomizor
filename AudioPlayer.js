9a64c37e405348d28a451973c81b067f624bd323
DellaSantaF <71725757+DellaSantaF@users.noreply.github.com>
2022-03-31 23:13:22 +0200

    working play-pause

--- AudioPlayer.js
+++ AudioPlayer.js
@@ -1,16 +1,28 @@
 //AudioPlayer class file
 
-const minInterval = 20*1000; //milliseconds
-const maxInterval = 120*1000; //milliseconds
+const minInterval = 1*1000; //milliseconds
+const maxInterval = 2*1000; //milliseconds
 
 
 class AudioPlayer { //Audio player, pauser and randomizer
     constructor(audiosList) {
         this.list = audiosList;
+        this.loop; //play-sleep loop
+        this.rd; //will be used as the audio object
+
+
+        document.getElementById('playLogo').innerHTML = logos[0].html;
+        document.getElementById('pauseLogo').innerHTML = logos[1].html;
 
         this.playButton = document.getElementById('playButton');
-
         this.playButton.addEventListener('click', this.playSound);
+
+        this.pauseButton = document.getElementById('pauseButton');
+        this.pauseButton.addEventListener('click', this.pause);
+
+        this.playClass = document.querySelectorAll('.play');
+        this.pauseClass = document.querySelectorAll('.pause');
+
     }
 
     randomElt() { //Gives a random element out of the list
@@ -23,13 +35,32 @@ class AudioPlayer { //Audio player, pauser and randomizer
     }
 
     playSound() {
-        setInterval ( function() {
-            let rd = player.randomElt(); //idk why but I have to use player instead of this
-            rd.play();
+
+        for (let i=0; i<player.playClass.length; i++) {
+            player.playClass[i].style.display = "none";
+        }
+        for (let i=0; i<player.pauseClass.length; i++) {
+            player.pauseClass[i].style.display = "block";
+        }
+
+        player.loop = setInterval ( function() {
+            player.rd = player.randomElt(); //idk why but I have to use player instead of this
+            player.rd.play();
         },minInterval + Math.random()*(maxInterval-minInterval))
     }
 
     pause() {
-        this.flag = false;
+        player.rd.pause();
+
+
+        for (let i=0; i<player.playClass.length; i++) {
+            player.playClass[i].style.display = "block";
+        }
+        for (let i=0; i<player.pauseClass.length; i++) {
+            player.pauseClass[i].style.display = "none";
+        }
+
+
+        clearInterval(player.loop);
     }
 }
