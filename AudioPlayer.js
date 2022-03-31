63ce1e2f09d395dab89f7b305aef888677f8e21a
DellaSantaF <71725757+DellaSantaF@users.noreply.github.com>
2022-03-31 16:09:27 +0200

    AudioPlayer.js

---
+++ AudioPlayer.js
@@ -0,0 +1,27 @@
+//AudioPlayer class file
+
+const minInterval = 20*1000; //milliseconds
+const maxInterval = 120*1000; //milliseconds
+
+
+class AudioPlayer { //Audio player, pauser and randomizer
+    constructor(audiosList) {
+        this.list = audiosList;
+        this.flag = true; //indicates wether the sound has to go on or stop
+    }
+
+    randomElt() { //Gives a random element out of the list
+        return this.list[Math.floor(Math.random()*this.list.length)];
+    }
+
+    play() {
+        while (this.flag) {
+            this.randomElt.play();
+            setTimeout(null, minInterval + Math.random()*(maxInterval-minInterval));
+        }
+    }     
+
+    pause() {
+        this.flag = false;
+    }
+}
