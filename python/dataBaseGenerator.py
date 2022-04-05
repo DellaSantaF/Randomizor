8885e2caafdd068322768b4a2c99fe0936d281f5
Helium <h2liumpond@gmail.com>
2022-04-05 19:33:53 +0200

    Create dataBaseGenerator.py
    dataBaseGenerator.py generates dataBase.js using sounds directory.
    The disable.txt files serves as a list of disabled sounds.

---
+++ python/dataBaseGenerator.py
@@ -0,0 +1,24 @@
+# Generates dataBase.js using sounds directory
+# Add mp3 filename to disable.txt to set its inList property to false
+
+import os
+
+dataBase = 'dataBase.js'
+
+with open(f'./python/disable.txt', 'r', encoding='utf-8') as ignoreList:
+    ignore = set()
+    for item in ignoreList.readlines():
+        ignore.add(item[:-1])
+
+print(ignore)
+with open(f'./javascript/{dataBase}', 'w', encoding='utf-8') as data:
+
+    data.write('//All mp3, svg data storage\n\nconst data = [\n')
+
+    for filename in os.listdir('./sounds'):
+        filename = filename[:-4]
+        inList = 'false' if filename in ignore else 'true'
+
+        data.write('    {' + f'name: "{filename}", inList: {inList}' + '},\n')
+
+    data.write(']')
