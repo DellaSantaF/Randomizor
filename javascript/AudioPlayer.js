73d1bbf90d4506930bc943f129c40f58197218db
Helium <h2liumpond@gmail.com>
2022-04-07 14:41:00 +0200

    Fix sounds continuing from where they were paused at in AudioPlayer.js

--- javascript/AudioPlayer.js
+++ javascript/AudioPlayer.js
@@ -1,6 +1,6 @@
 // AudioPlayer class file
 const MIN_INTERVAL = 0*1000; // milliseconds
-let MAX_INTERVAL = 600*1000; // milliseconds
+let max_interval = 600*1000; // milliseconds
 
 document.getElementById('playLogo').innerHTML = logos[0].html;
 document.getElementById('pauseLogo').innerHTML = logos[1].html;
@@ -8,15 +8,16 @@ document.getElementById('pauseLogo').innerHTML = logos[1].html;
 //TODO: replace all instances of player with this.
 class AudioPlayer { //Audio player, pauser and randomizer
     constructor(audioList) {
+        const self = this;
         this.audioList = audioList;
         this.loop; // play-sleep loop
         this.currentAudio; // will be used as the audio object
 
         this.playButton = document.getElementById('playButton');
-        this.playButton.addEventListener('click', this.playSound);
+        this.playButton.addEventListener('click', () => {this.play()});
 
         this.pauseButton = document.getElementById('pauseButton');
-        this.pauseButton.addEventListener('click', this.pause);
+        this.pauseButton.addEventListener('click', () => {this.pause()});
 
         this.playClass = document.querySelectorAll('.play');
         this.pauseClass = document.querySelectorAll('.pause');
@@ -24,10 +25,10 @@ class AudioPlayer { //Audio player, pauser and randomizer
         this.timer = document.getElementById('timer');
 
         this.secretButton = document.getElementById('secretButton');
-        this.secretButton.addEventListener('click', () => {this.randomElt().play();})
+        this.secretButton.addEventListener('click', () => {this.randomSound()})
 
-        this.intervalChange = document.getElementById('interval');
-        this.intervalChange.addEventListener('change', () => {MAX_INTERVAL = Number(this.intervalChange.value)*60*1000});
+        this.intervalInput = document.getElementById('interval');
+        this.intervalInput.addEventListener('change', () => {max_interval = Number(this.intervalInput.value)*60*1000});
     }
 
     randomElt() { // Gives a random element out of the audioList
@@ -38,28 +39,30 @@ class AudioPlayer { //Audio player, pauser and randomizer
         return this.audioList[index]
     }
 
-    playSound() {
-        for (const path of player.playClass) {
+    randomSound() {
+        this.randomElt().play();
+    }
+
+    play() {
+        for (const path of this.playClass) {
             path.style.display = "none";
         }
-        for (const path of player.pauseClass) {
+        for (const path of this.pauseClass) {
             path.style.display = "block";
         }
 
-        player.randomSoundLoop()
-
-        player.startTimer()
+        this.randomSoundLoop()
+        this.startTimer()
     }
 
     randomSoundLoop() {
-        player.loop = setTimeout (
+        this.loop = setTimeout (
             () => {
-                player.currentAudio = player.randomElt();
-                player.currentAudio.play();
+                this.randomSound()
 
-                player.randomSoundLoop(); // recursive call
+                this.randomSoundLoop(); // recursive call
             }
-            ,MIN_INTERVAL + Math.random()*(MAX_INTERVAL-MIN_INTERVAL)
+            ,MIN_INTERVAL + Math.random()*(max_interval-MIN_INTERVAL)
         )
     }
 
@@ -70,7 +73,7 @@ class AudioPlayer { //Audio player, pauser and randomizer
         let minutes = 0;
         let hours = 0;
 
-        player.timerLoop = setInterval(
+        this.timerLoop = setInterval(
             () => {
                 this.timer.textContent = `\
                 ${hours < 10? '0' + hours : hours} :
@@ -89,19 +92,20 @@ class AudioPlayer { //Audio player, pauser and randomizer
     }
 
     pause() {
-        player.audioList.forEach((audio) => {
+        this.audioList.forEach((audio) => {
             audio.pause();
+            audio.currentTime = 0;
         })
 
-        for (const path of player.playClass) {
+        for (const path of this.playClass) {
             path.style.display = "block";
         }
-        for (const path of player.pauseClass) {
+        for (const path of this.pauseClass) {
             path.style.display = "none";
         }
 
-        clearTimeout(player.loop);
-        clearInterval(player.timerLoop);
+        clearTimeout(this.loop);
+        clearInterval(this.timerLoop);
     }
 
     static audioByID(id) {
