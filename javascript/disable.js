13f9eb7cc59ee4238baa62f02400b795f673dcdf
Helium <h2liumpond@gmail.com>
2022-04-07 15:19:05 +0200

    Polish dataBaseGenerator.py

---
+++ javascript/disable.js
@@ -0,0 +1,16 @@
+// audio files disabled by default.
+
+disable = [
+    "gamme",
+    "jépapété",
+    "photo",
+    "ogpt2",
+    "ogpt3",
+    "realman",
+    "photo",
+    "stegosaurus",
+    "trompet",
+    "beepsheep",
+    "cvedir",
+    "davidG",
+]
--- python/dataBaseGenerator.py
+++ python/dataBaseGenerator.py
@@ -4,17 +4,7 @@
 from os import listdir
 
 dataBase = './javascript/dataBase.js'
-disable = './javascript/disable.js'
-
-with open('./python/disable.txt', 'r', encoding='utf-8') as ignoreList, open(disable, 'w', encoding='utf-8') as disable:
-    disable.write("""// audio files disabled by default.
-
-disable = [
-""")
-    for filename in ignoreList.readlines()[3:]:
-        disable.write(f'    "{filename[:-1]}",\n')
-
-    disable.write(']')
+ignore = './javascript/disable.js'
 
 with open(dataBase, 'w', encoding='utf-8') as data:
 
@@ -27,3 +17,17 @@ const data = [
         data.write(f'    "{filename[:-4]}",\n')
 
     data.write(']')
+
+print(f'{dataBase} has been successfully updated')
+
+with open('./python/disable.txt', 'r', encoding='utf-8') as ignoreList, open(ignore, 'w', encoding='utf-8') as disable:
+    disable.write("""// audio files disabled by default.
+
+disable = [
+""")
+    for filename in ignoreList.readlines()[3:]:
+        disable.write(f'    "{filename[:-1]}",\n')
+
+    disable.write(']')
+
+print(f'{ignore} has been successfully updated')
